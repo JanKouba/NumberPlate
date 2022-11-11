6f7fe56371305463b3e57d025f401c1e253ad9d3
Ferda Mravenec <koubisk@seznam.cz>
2022-11-11 17:23:44 +0100

    EEPROM ability, wifi modes, broadcasting (still problematic)

--- NumberPlate/s2mini.ino
+++ NumberPlate/s2mini.ino
@@ -2,8 +2,12 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <EEPROM.h>
+#include <AsyncUDP.h>
 
 #define LED 15
+
+AsyncUDP udp;
 
 bool stav = false;
 int treshold = 25;
@@ -12,14 +16,27 @@ bool result = false;
 
 bool connected = false;
 
-char ssid[50]= "netis_78B5E8";
+String wifiMode = "none";
+
+char ssid[50]= "cedule";
 char password[50] = "password";
 
 int opakovani = 0;
 
 IPAddress ip;
+IPAddress localIp (192,168,0,1);
+IPAddress subnet (255,255,255,0);
+IPAddress gateWay (192,168,0,1);
+
+IPAddress broadcast (192,168,0,2);
 
 WebServer server(8081);
+
+int mem_ssid = 0;
+int mem_pass = 0;
+int mem_mode = 0;
+int mem_value = 0;
+int mem_tresh = 0;
 
 void handleRoot() {
 
@@ -46,7 +63,19 @@ bool InitWifi() {
   return result;
 }
 
-bool Connect() {
+bool Connect() 
+{
+  if (wifiMode == "client")
+      result = ClientMode();
+  else if(wifiMode == "ap")
+      result = APMode();      
+
+  connected = result;
+  return result;
+}
+
+bool ClientMode()
+{
 
   bool result = false;
 
@@ -60,10 +89,12 @@ bool Connect() {
   WiFi.begin(ssid, password);
 
   opakovani = 0;
-
+  delay(1000);
+  
   while (WiFi.status() != WL_CONNECTED && opakovani < 3) {
     delay(500);
     Serial.print(".");
+    WiFi.begin(ssid, password);
     opakovani++;
   }
 
@@ -79,8 +110,36 @@ bool Connect() {
       result = true;
   }
 
-  connected = result;
   return result;
+
+}
+
+bool APMode()
+{
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Vytvoreni site ");
+  Serial.println(ssid);
+  Serial.print("Heslo ");
+  Serial.println(password);
+
+  WiFi.mode(WIFI_AP);
+
+  WiFi.softAPConfig(localIp, gateWay, subnet);
+
+  result = WiFi.softAP(ssid,password);
+
+  if (result)
+    ip = localIp;
+  else
+    ip = {0,0,0,0};
+
+  Serial.println("IP adresa: ");
+  Serial.println(ip);
+
+  return result;
+  
 }
 
 bool StartHttp() {
@@ -124,11 +183,20 @@ void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(115200);
 
+  EEPROM.begin(1024);
+
+  CalculateMemoryOffset();
+  LoadSettings();
+
   digitalWrite(LED, true);
 
   if (InitWifi())
     if (Connect())
+    {
+      /*udp.connect(IPAddress(255,255,255,255), 8061);*/
+      GetBroadcast();
       StartHttp();
+    }
 }
 
 void loop() {
@@ -139,6 +207,37 @@ void loop() {
 
   Display();
   delay(10);  
+}
+
+void CallBroadcast()
+{
+  //udp.broadcast("tralala");
+  Serial.println("Broadcasting...");
+}
+
+void GetBroadcast()
+{
+  if(udp.listen(8061)) {
+  udp.onPacket([](AsyncUDPPacket packet) {
+            Serial.print("UDP Packet Type: ");
+            Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+            Serial.print(", From: ");
+            Serial.print(packet.remoteIP());
+            Serial.print(":");
+            Serial.print(packet.remotePort());
+            Serial.print(", To: ");
+            Serial.print(packet.localIP());
+            Serial.print(":");
+            Serial.print(packet.localPort());
+            Serial.print(", Length: ");
+            Serial.print(packet.length());
+            Serial.print(", Data: ");
+            Serial.write(packet.data(), packet.length());
+            Serial.println();
+            //reply to the client
+            packet.printf("Got %u bytes of data", packet.length());
+        });
+  }
 }
 
 void Display()
@@ -168,7 +267,52 @@ void Blinking()
     stav = !stav;
     digitalWrite(LED, stav);
     lastTime = currTime;
+    
+    /*
+    if (connected && stav)
+      GetBroadcast();
+      */
   }
+
+}
+
+void CalculateMemoryOffset()
+{
+  mem_ssid = 0;
+  mem_pass = mem_ssid + 32 + 1;
+  mem_mode = mem_pass + 64 + 1;
+  mem_value = mem_mode + 6 + 1;
+  mem_tresh = mem_value + 4 + 1;
+}
+
+void LoadSettings()
+{
+   String eepromValue = "";
+
+     //SSID
+   eepromValue = readStringFromEEPROM(mem_ssid);
+   eepromValue.toCharArray(ssid, eepromValue.length()+1);
+   Serial.println(ssid);
+
+     //Password
+   eepromValue = readStringFromEEPROM(mem_pass);
+   eepromValue.toCharArray(password, eepromValue.length()+1);
+   Serial.println(password);
+   
+     //Wifi Mode
+   eepromValue = readStringFromEEPROM(mem_mode);
+   wifiMode = eepromValue;
+   Serial.println(wifiMode);
+
+     //Value
+   eepromValue = readStringFromEEPROM(mem_value);
+   value = eepromValue.toInt();
+   Serial.println(value);
+
+     //Treshold
+   eepromValue = readStringFromEEPROM(mem_tresh);
+   treshold = eepromValue.toInt();
+   Serial.println(treshold);
 
 }
 
@@ -259,6 +403,7 @@ String Execute(String command, String argument)
     if (argLen > 0)
     {
       argument.toCharArray(ssid, argument.length()+1);
+      writeStringToEEPROM(mem_ssid, argument);
     }
 
     rtnMsg = "SSID is set to: ";
@@ -269,6 +414,7 @@ String Execute(String command, String argument)
     if (argLen > 0)
     {
       argument.toCharArray(password, argument.length()+1);
+      writeStringToEEPROM(mem_pass, argument);
     }
 
     rtnMsg = "Password is set to: ";
@@ -278,7 +424,10 @@ String Execute(String command, String argument)
   {
     if (argLen > 0)
     if (StringIsNumber(argument))
+    {
       treshold = argument.toInt();
+      writeStringToEEPROM(mem_tresh, argument);
+    }
 
     rtnMsg = "Treshold is set to: ";
     rtnMsg.concat(String(treshold));
@@ -287,7 +436,10 @@ String Execute(String command, String argument)
   {
     if (argLen > 0)
       if (StringIsNumber(argument))
+      {
         value = argument.toInt();
+        writeStringToEEPROM(mem_value, argument);
+      }
 
     rtnMsg = "Value is set to: ";
     rtnMsg.concat(String(value));
@@ -296,12 +448,16 @@ String Execute(String command, String argument)
   {
     value = value + 1;
 
+    writeStringToEEPROM(mem_value, String(value));
+
     rtnMsg = "Value is set to: ";
     rtnMsg.concat(String(value));
   }
   else if (command.equals("deduct") || command.equals("minus") || command.equals("-"))
   {
     value = value - 1;
+
+    writeStringToEEPROM(mem_value, String(value));
 
     rtnMsg = "Value is set to: ";
     rtnMsg.concat(String(value));
@@ -317,10 +473,25 @@ String Execute(String command, String argument)
     connected = false;
     rtnMsg = "Disconnected";
   }
+  else if (command.equals("wifimode"))
+  {
+    writeStringToEEPROM(mem_mode, argument);
+    wifiMode = argument;
+    rtnMsg = "Value is set to: ";
+    rtnMsg.concat(String(wifiMode));
+  }
+
+  else if (command.equals("load"))
+  {
+    LoadSettings();
+  }
+
   else if(command.equals("status"))
   {
     rtnMsg  = "conn:";
     rtnMsg.concat(String(connected));
+    rtnMsg.concat("|wifimode:");
+    rtnMsg.concat(wifiMode);
     rtnMsg.concat("|ssid:");
     rtnMsg.concat(ssid);
     rtnMsg.concat("|password:");
@@ -333,6 +504,8 @@ String Execute(String command, String argument)
     rtnMsg.concat(String(ip[2]));
     rtnMsg.concat(".");
     rtnMsg.concat(String(ip[3]));
+    rtnMsg.concat("|mac:");
+    rtnMsg.concat(String(WiFi.macAddress()));
     rtnMsg.concat("|treshold:");
     rtnMsg.concat(String(treshold));
     rtnMsg.concat("|value:");
@@ -357,4 +530,34 @@ bool StringIsNumber(String inputString)
       retVal = false;
 
   return retVal;
+}
+
+void writeStringToEEPROM(int addrOffset, const String &strToWrite)
+{
+  byte len = strToWrite.length();
+  EEPROM.write(addrOffset, len);
+  
+  for (int i = 0; i < len; i++)
+  {
+    EEPROM.write(addrOffset + 1 + i, strToWrite[i]);
+  }
+
+  EEPROM.commit();
+
+}
+
+String readStringFromEEPROM(int addrOffset)
+{
+  int newStrLen = EEPROM.read(addrOffset);
+
+  Serial.println(String(newStrLen));
+
+  char data[newStrLen + 1];
+  for (int i = 0; i < newStrLen; i++)
+  {
+    data[i] = EEPROM.read(addrOffset + 1 + i);
+  }
+  
+  data[newStrLen] = '\0';
+  return String(data);
 }
